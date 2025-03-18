304c9f7570d066654d4ada83ec8894ef7388fe03
Falcon-16 <raykohlhepp@outlook.com>
2025-03-18 14:10:52 +1000

    removed broken login link

--- logout.js
+++ logout.js
@@ -3,7 +3,7 @@ firebase.initializeApp(firebaseConfig);
 function logout() {
   firebase.auth().signOut().then(() => {
       console.log('User signed out successfully');
-      window.location.href = 'login.html'; // Redirect to login page
+      window.location.href = 'Study-Notes/index.html'; // Redirect to login page
   }).catch((error) => {
       console.error('Logout error:', error);
   });

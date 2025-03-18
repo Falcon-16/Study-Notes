5a27b50d8cfa7731f55924945483c5b81ec04a48
Falcon-16 <raykohlhepp@outlook.com>
2025-03-18 12:49:07 +1000

    debugged logout and signup

--- logout.js
+++ logout.js
@@ -1,3 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9-tIR3vEwjXQHtELGWhGRxOuS9xbZTvA",
+  authDomain: "github-pages-ec8ac.firebaseapp.com",
+  projectId: "github-pages-ec8ac",
+  storageBucket: "github-pages-ec8ac.firebasestorage.app",
+  messagingSenderId: "552576250177",
+  appId: "1:552576250177:web:1cc965f999b1045ee0b1c0"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 function logout() {
     firebase.auth().signOut().then(() => {
       console.log('User signed out successfully');
